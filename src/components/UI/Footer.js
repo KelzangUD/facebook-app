acee2ee980d1830b9beaef2a97e71c708d338134
Kelzang Ugyen Dorji <kuding755@email.com>
2022-03-24 16:55:46 +0600

    footer design completed

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -1,58 +1,29 @@
 import classes from './Footer.module.css';
+import languageData from '../../data/footerdata/Language.json';
+import additonalData from '../../data/footerdata/additional.json';
 
 const Footer = () =>{
+    let theLangData = languageData;
+    let additional = additonalData;
     return (
         <div className={classes.footer}>
             <div className={classes.footerContent}>
                 <div className={classes.pageFooter}>
                     <ul>
                         <li>English (UK)</li>
-                        <li><a class="_sv4" dir="ltr" href="https://pt-br.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;pt_BR&quot;, &quot;en_GB&quot;, &quot;https:\/\/pt-br.facebook.com\/&quot;, &quot;www_list_selector&quot;, 0); return false;" title="Portuguese (Brazil)">Português (Brasil)</a></li>
-                        <li><a class="_sv4" dir="ltr" href="https://es-la.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;es_LA&quot;, &quot;en_GB&quot;, &quot;https:\/\/es-la.facebook.com\/&quot;, &quot;www_list_selector&quot;, 1); return false;" title="Spanish">Español</a></li>
-                        <li><a class="_sv4" dir="ltr" href="https://ja-jp.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;ja_JP&quot;, &quot;en_GB&quot;, &quot;https:\/\/ja-jp.facebook.com\/&quot;, &quot;www_list_selector&quot;, 2); return false;" title="Japanese">日本語</a></li>
-                        <li><a class="_sv4" dir="ltr" href="https://fr-fr.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;fr_FR&quot;, &quot;en_GB&quot;, &quot;https:\/\/fr-fr.facebook.com\/&quot;, &quot;www_list_selector&quot;, 3); return false;" title="French (France)">Français (France)</a></li>
-                        <li><a class="_sv4" dir="ltr" href="https://th-th.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;th_TH&quot;, &quot;en_GB&quot;, &quot;https:\/\/th-th.facebook.com\/&quot;, &quot;www_list_selector&quot;, 4); return false;" title="Thai">ภาษาไทย</a></li>
-                        <li><a class="_sv4" dir="ltr" href="https://de-de.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;de_DE&quot;, &quot;en_GB&quot;, &quot;https:\/\/de-de.facebook.com\/&quot;, &quot;www_list_selector&quot;, 5); return false;" title="German">Deutsch</a></li>
-                        <li><a class="_sv4" dir="ltr" href="https://id-id.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;id_ID&quot;, &quot;en_GB&quot;, &quot;https:\/\/id-id.facebook.com\/&quot;, &quot;www_list_selector&quot;, 6); return false;" title="Indonesian">Bahasa Indonesia</a></li>
-                        <li><a class="_sv4" dir="ltr" href="https://it-it.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;it_IT&quot;, &quot;en_GB&quot;, &quot;https:\/\/it-it.facebook.com\/&quot;, &quot;www_list_selector&quot;, 7); return false;" title="Italian">Italiano</a></li>
-                        <li><a class="_sv4" dir="rtl" href="https://ar-ar.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;ar_AR&quot;, &quot;en_GB&quot;, &quot;https:\/\/ar-ar.facebook.com\/&quot;, &quot;www_list_selector&quot;, 8); return false;" title="Arabic">العربية</a></li>
-                        <li><a class="_sv4" dir="ltr" href="https://hi-in.facebook.com/" onclick="require(&quot;IntlUtils&quot;).setCookieLocale(&quot;hi_IN&quot;, &quot;en_GB&quot;, &quot;https:\/\/hi-in.facebook.com\/&quot;, &quot;www_list_selector&quot;, 9); return false;" title="Hindi">हिन्दी</a></li>
-                        <li><a role="button" class="_42ft _4jy0 _517i _517h _51sy" rel="dialog" ajaxify="/settings/language/language/?uri=https%3A%2F%2Fhi-in.facebook.com%2F&amp;source=www_list_selector_more" href="#" title="Show more languages"><i className="bi bi-plus-square-fill"></i></a></li>
+                        {theLangData.map(language => (
+                                (<li key={language.title}><a href={`https://${language.href}.facebook.com/`}  title={language.title}>{language.language}</a></li>)
+                        ))}
+                        <li><a role="button"  href="#" title="Show more languages"><i id="iconFooter" className="bi bi-plus-square-fill"></i></a></li>
                     </ul>
                 </div>
                 <hr/>
                 <div className={classes.pageFooter}>
                     <ul>
-                        <li><a href="/reg/" title="Sign up for Facebook">Sign Up</a></li>
-                        <li><a href="/login/" title="Log in to Facebook">Log In</a></li>
-                        <li><a href="https://messenger.com/" title="Take a look at Messenger.">Messenger</a></li>
-                        <li><a href="/lite/" title="Facebook Lite for Android.">Facebook Lite</a></li>
-                        <li><a href="https://www.facebook.com/watch/" title="Browse our Watch videos.">Watch</a></li>
-                        <li><a href="/places/" title="Take a look at popular places on Facebook.">Places</a></li>
-                        <li><a href="/games/" title="Check out Facebook games.">Games</a></li>
-                        <li><a href="/marketplace/" title="Buy and sell on Facebook Marketplace.">Marketplace</a></li>
-                        <li><a href="https://pay.facebook.com/" title="Learn more about Facebook Pay" target="_blank">Facebook Pay</a></li>
-                        <li><a href="https://www.oculus.com/" title="Learn more about Oculus" target="_blank">Oculus</a></li>
-                        <li><a href="https://portal.facebook.com/" title="Learn more about Facebook Portal" target="_blank">Portal</a></li>
-                        <li><a href="https://www.instagram.com/" title="Take a look at Instagram" target="_blank" rel="noopener nofollow" data-lynx-mode="asynclazy" data-lynx-uri="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2F&amp;h=AT1lWDHCvrw1iiHfM1VHQ1-rgp0w_iKqc-MswRUW2L7Wvi7seP2rTbfFdSch5_oDx1jNNOhIgj4OLg9HV01ibYuco5S1ZhuZ7hoVHLVlVIfe7imEGkXwzheOcozTnPNiYfwTCFNZ82R-LhQ26TV7-Q">Instagram</a></li>
-                        <li><a href="https://www.bulletin.com/" title="Take a look at Bulletin newsletter">Bulletin</a></li>
-                        <li><a href="/local/lists/245019872666104/" title="Browse our Local Lists directory.">Local</a></li>
-                        <li><a href="/fundraisers/" title="Donate to worthy causes.">Fundraisers</a></li>
-                        <li><a href="/biz/directory/" title="Browse our Facebook Services directory.">Services</a></li>
-                        <li><a href="/votinginformationcenter/?entry_point=c2l0ZQ%3D%3D" title="See the Voting Information Centre">Voting Information Centre</a></li>
-                        <li><a href="/groups/explore/" title="Explore our groups.">Groups</a></li>
-                        <li><a href="https://about.facebook.com/" accesskey="8" title="Read our blog, discover the resource centre and find job opportunities.">About</a></li>
-                        <li><a href="/ad_campaign/landing.php?placement=pflo&amp;campaign_id=402047449186&amp;nav_source=unknown&amp;extra_1=auto" title="Advertise on Facebook">Create ad</a></li>
-                        <li><a href="/pages/create/?ref_type=site_footer" title="Create a Page">Create Page</a></li>
-                        <li><a href="https://developers.facebook.com/?ref=pf" title="Develop on our platform.">Developers</a></li>
-                        <li><a href="/careers/?ref=pf" title="Make your next career move to our brilliant company.">Careers</a></li>
-                        <li><a data-nocookies="1" href="/privacy/explanation/" title="Learn about your privacy and Facebook.">Privacy</a></li>
-                        <li><a href="/policies/cookies/" title="Learn about cookies and Facebook." data-nocookies="1">Cookies</a></li>
-                        <li><a class="_41ug" data-nocookies="1" href="https://www.facebook.com/help/568137493302217" title="Learn about AdChoices.">AdChoices<i className="bi bi-backspace-reverse"></i></a></li>
-                        <li><a data-nocookies="1" href="/policies?ref=pf" accesskey="9" title="Review our terms and policies.">Terms</a></li>
-                        <li><a href="/help/?ref=pf" accesskey="0" title="Visit our Help Centre.">Help</a></li>
-                        <li><a accesskey="6" class="accessible_elem" href="/settings" title="View and edit your Facebook settings.">Settings</a></li>
-                        <li><a accesskey="7" class="accessible_elem" href="/allactivity?privacy_source=activity_log_top_menu" title="View your activity log">Activity log</a></li>
+                        {
+                            additional.map(add=>(
+                            <li key={add.href}><a href={add.href} title={add.title}>{add.name}</a></li>))
+                        }
                     </ul>
                 </div>
                 <div>
@@ -62,7 +33,6 @@ const Footer = () =>{
                 </div>
             </div>
             <div>
-
             </div>
         </div>
     );
